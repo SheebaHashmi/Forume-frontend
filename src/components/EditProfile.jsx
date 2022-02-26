b767b16215f4f99fa949c825dec817746f12a17b
SheebaHashmi <sheebahashmi0@gmail.com>
2022-02-26 12:55:56 -0500

    Finish styling according to Figma

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -3,59 +3,64 @@ import { Link } from 'react-router-dom';
 function EditProfile() {
     return (
         <div className="forum-container">
-            <div className="forum-content"> 
+            <div className="forum-content">
 
-            <h1 className="text-center">Edit Profile</h1>
-            <form className="container card col-10 col-md-8 offset-md-2 my-5 p-4">
-                <div className="row">
-                    <div className="col-12 col-md-6 mb-2 offset-md-3">
-                        <label className="form-label">Fullname: </label>
-                        <input
-                            type="text"
-                            name="fullname"
-                            className="form-control"
-                        />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-12 col-md-6 mb-2 offset-md-3">
-                        <label className="form-label">Email: </label>
-                        <input
-                            type="text"
-                            name="email"
-                            className="form-control"
-                        />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-12 col-md-6 mb-2 offset-md-3">
-                        <label className="form-label">Username: </label>
-                        <input
-                            type="text"
-                            name="username"
-                            className="form-control"
-                        />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-12 col-md-6 mb-2 offset-md-3">
-                        <label className="form-label">Password: </label>
-                        <input
-                            type="text"
-                            name="password"
-                            className="form-control"
-                        />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-12 col-md-3 offset-md-5 mt-3">
-                        <Link to={'/profile'}>
-                            <button className="btn btn-primary px-5" type="submit">Submit</button>
-                        </Link>
-                    </div>
-                </div>
+                <h1 className="text-center fw-bolder">Edit Profile</h1>
+                <form className="container card col-10 col-md-8 offset-md-2 my-5 p-4">
 
-            </form>
+                    <div className="row">
+                        <div className="col-12 col-md-6 mb-2 offset-md-3">
+                            <label className="form-label fw-bold">Fullname: </label>
+                            <input
+                                type="text"
+                                name="fullname"
+                                className="form-control"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-12 col-md-6 mb-2 offset-md-3">
+                            <label className="form-label fw-bold">Email: </label>
+                            <input
+                                type="text"
+                                name="email"
+                                className="form-control"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-12 col-md-6 mb-2 offset-md-3">
+                            <label className="form-label fw-bold">Username: </label>
+                            <input
+                                type="text"
+                                name="username"
+                                className="form-control"
+                            />
+                        </div>
+
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-6 mb-2 offset-md-3">
+                            <label className="form-label fw-bold">Password: </label>
+                            <input
+                                type="text"
+                                name="password"
+                                className="form-control"
+                            />
+                        </div>
+                        
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-3 offset-md-5 mt-3">
+                            <Link to={'/profile'}>
+                                <button className="btn btn-primary px-5" type="submit">Submit</button>
+                            </Link>
+                        </div>
+                    </div>
+
+                </form>
             </div>
         </div>
     );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,18 +4,18 @@ function Profile() {
         <div className="forum-container">
             <div className="forum-content">
 
-            <h1 className="text-center">User Profile</h1>
-            <div className="card col-12 col-md-8 offset-md-2 my-5 p-4">
-                <div className="card-body">
-                        <h4>Username: Blaise</h4>
-                        <h4>Fullname: Makebelieve flamming Blaise</h4>
+                <h1 className="text-center fw-bolder">User Profile</h1>
+
+                <div className="container card col-10 col-md-8 offset-md-2 my-5 py-5 ">
+                    <div className="card-body">
+                        <h4 className="fw-bold">Username: Blaise</h4>
+                        <h4 className="fw-bold">Fullname: Blaiso kittywfafasf</h4>
                         <Link to={'/editProfile'}>
                             <button className="btn btn-primary px-5 mt-4">Edit Profile</button>
                         </Link>
+                    </div>
 
                 </div>
-
-            </div>
             </div>
         </div>
     )
